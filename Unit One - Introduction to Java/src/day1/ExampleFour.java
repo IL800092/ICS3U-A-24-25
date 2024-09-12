9caa2649fa09c7c64d84a4e577ec32b06228925b
Ian Luk <iluk@bayviewglen.ca>
2024-09-12 10:39:29 -0400

    September12

---
+++ Unit One - Introduction to Java/src/day1/ExampleFour.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class ExampleFour {
+    public static void main(String[] args) {
+        final int x = 7;
+        int y = 8;
+        y = 9;
+        
+    }
+}
---
+++ Unit One - Introduction to Java/src/day1/ExampleTwo.java
@@ -0,0 +1,12 @@
+package day1;     // semi-colon is the end of a statement
+
+public class ExampleTwo {       // {} are used to indicate something
+                                // is embedded (inside) something
+    public static void main(String[] args) {
+        System.out.println("This is harder than python");
+        System.out.println("Sysout makes it faster"); // use sysout to increase speed
+        System.out.println();
+        System.out.println("A");
+        System.out.print("B");
+    }
+}
